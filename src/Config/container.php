42a0f49f702a07f3f00e93fef1031c2e8afc6ff1
Oriol Sastre Rienitz <oriol.sastre@gmail.com>
2025-01-25 12:35:23 +0100

    fix

--- src/Config/container.php
+++ src/Config/container.php
@@ -12,7 +12,7 @@ use function DI\create;
 $container_bindings = [
     Config::class => create(Config::class)->constructor($_ENV),
     EntityManager::class => function (Config $config) {
-        $doctConfig = ORMSetup::createAttributeMetadataConfiguration([__DIR__ . '/../src/Models']);
+        $doctConfig = ORMSetup::createAttributeMetadataConfiguration(paths: [__DIR__ . '/../src/Models'], isDevMode: true); // $isDevMode=true ja que sinó tinc problemes de Redis al deployment. 
         $doctConfig->setAutoGenerateProxyClasses(false);
         return new EntityManager(
             DriverManager::getConnection($config->db),
