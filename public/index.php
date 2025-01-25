316f64bb80d8b5b7ffe507b61babd67aed0376db
Oriol Sastre Rienitz <oriol.sastre@gmail.com>
2025-01-25 11:36:21 +0100

    fix

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Kartalit\Controllers\AutorController;
 use Kartalit\Controllers\EsdevenimentController;
 
-$app = require_once dirname(__DIR__) . '/bootstrap.php';
+$app = require_once __DIR__ . '/../bootstrap.php';
 
 $app->setBasePath($_ENV['ENV_SERVER_BASEPATH'] ?? '/');
 
